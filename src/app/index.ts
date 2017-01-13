7fc61a37907ef41ef116ead97a074a36e532dfc6
avolkova <anna_volkova@tut.by>
2017-01-13 15:57:41 +0300

    Modal video added

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,22 +2,26 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {UIView, UIRouterModule} from 'ui-router-ng2';
 import {STATES, MyUIRouterConfig} from './routes';
+import {ModalModule} from 'ng2-bootstrap/modal';
 
 import {MainComponent} from './main';
 import {HeaderComponent} from './header';
 import {FooterComponent} from './footer';
 import {PageComponent} from './page';
+import {VideoModalComponent} from './video_modal';
 
 @NgModule({
   imports: [
     BrowserModule,
+    ModalModule.forRoot(),
     UIRouterModule.forRoot({states: STATES, configClass: MyUIRouterConfig})
   ],
   declarations: [
     PageComponent,
     MainComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    VideoModalComponent
   ],
   bootstrap: [UIView]
 })
---
+++ src/app/video_modal.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'videol',
+  template: require('./video_modal.html')
+})
+export class VideoModalComponent {
+
+}
