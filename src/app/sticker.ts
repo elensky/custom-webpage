bc7c94a7a4b5e081fabc4d3b83df38004679d590
Alexander Elensky <a.elensky@gastrofix.com>
2017-01-13 19:32:24 +0300

    Improved sticker

--- src/app/sticker.ts
+++ src/app/sticker.ts
@@ -1,19 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'sticker',
   template: require('./sticker.html')
 })
 export class StickerComponent {
-
+  @Input() sticker: Sticker;
 }
 
 export class Sticker {
   id: string;
   name: string;
+  image: string;
+  size: string;
 
-  constructor(id: string, name: string) {
+  constructor(id: string, name: string, image: string, size: string) {
     this.id = id;
     this.name = name;
+    this.image = image;
+    this.size = size;
   }
 }
