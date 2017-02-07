67e3263663d82969771b6eccac03224b8b2be781
avolkova <anna_volkova@tut.by>
2017-02-07 12:16:21 +0300

    Displaying online shops implemented

--- src/app/buy_modal.ts
+++ src/app/buy_modal.ts
@@ -1,6 +1,8 @@
 import {Component, ViewChild, ElementRef} from '@angular/core';
-
 import {Sticker} from './sticker';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Address} from './dropdown';
 
 enum ModalMode {
   online,
@@ -18,9 +20,17 @@ export class BuyModalComponent {
   sticker: Sticker;
   modalMode = ModalMode;
   public mode: ModalMode;
+  onlineAddresses: [Address];
+
+  constructor (private http: Http) {}
 
   showModal = function (type: ModalMode) {
     this.mode = type;
     this.modal.show();
+    if (!this.onlineAddresses) {
+      this.http.get('online_shops.json')
+        .map(res => res.json())
+        .subscribe(onlineAddresses => this.onlineAddresses = onlineAddresses);
+    }
   };
 }
