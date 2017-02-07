68ead74c5d44c9bf5d8a8dc333bee294825a3ccc
Alexander Elensky <a.elensky@gastrofix.com>
2017-02-07 19:08:10 +0300

    Hide logo on main page

--- src/app/header.ts
+++ src/app/header.ts
@@ -1,6 +1,15 @@
 import {Component} from '@angular/core';
+import {UIRouter} from 'ui-router-ng2';
+
 
 @Component({
   template: require('./header.html')
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  constructor(private uiRouter: UIRouter) {}
+
+  isShownMainLogo() {
+    return this.uiRouter.globals.current.name != 'page.main';
+  }
+}
